71b30a8645763b885f8980395c8ce359c436a55a
EADARASURESH <eadara.suresh@gmail.com>
2020-06-21 14:41:05 +0000

    Replace multiple string builders

--- Royal.Insurance.Renewal.UIApplication/Models/GetTextService.cs
+++ Royal.Insurance.Renewal.UIApplication/Models/GetTextService.cs
@@ -30,16 +30,16 @@ namespace Royal.Insurance.Renewal.UIApplication.Models
                 string text = File.ReadAllText(objpath);
                 StringBuilder sb = new StringBuilder();
                 sb.Append(text);
-                sb.Replace(Constant.CurrentDate, DateTime.Now.Date.ToString(Constant.DateFormat));
-                sb.Replace(Constant.FullName, outPutDto.Title + " " + outPutDto.FirstName);
-                sb.Replace(Constant.WitthSurName, outPutDto.Title + " " + outPutDto.FirstName + " " + outPutDto.Surname);
-                sb.Replace(Constant.PoductName, outPutDto.ProductName);
-                sb.Replace(Constant.PayOutAmount, Constant.Pound + outPutDto.PayOutAmount.ToString(CultureInfo.InvariantCulture));
-                sb.Replace(Constant.AnnualPremiumCharge, Constant.Pound + Math.Round(outPutDto.AnnualPemium,2).ToString());
-                sb.Replace(Constant.CreditCharge, Constant.Pound + Math.Round(outPutDto.CreditCharge,2).ToString());
-                sb.Replace(Constant.TotalPremium, Constant.Pound +Math.Round(outPutDto.TotalPremium,2).ToString());
-                sb.Replace(Constant.InitilaMonthPremium, Constant.Pound + outPutDto.InitialMonthlyPaymentAmount.ToString(CultureInfo.InvariantCulture));
-                sb.Replace(Constant.OtherMonthPremium, Constant.Pound + outPutDto.OtherMonthlyPaymentsAmount.ToString(CultureInfo.InvariantCulture));
+                sb.Replace(Constant.CurrentDate, DateTime.Now.Date.ToString(Constant.DateFormat))
+                .Replace(Constant.FullName, outPutDto.Title + " " + outPutDto.FirstName)
+                .Replace(Constant.WitthSurName, outPutDto.Title + " " + outPutDto.FirstName + " " + outPutDto.Surname)
+                .Replace(Constant.PoductName, outPutDto.ProductName)
+                .Replace(Constant.PayOutAmount, Constant.Pound + outPutDto.PayOutAmount.ToString(CultureInfo.InvariantCulture))
+                .Replace(Constant.AnnualPremiumCharge, Constant.Pound + Math.Round(outPutDto.AnnualPemium, 2).ToString())
+                .Replace(Constant.CreditCharge, Constant.Pound + Math.Round(outPutDto.CreditCharge, 2).ToString())
+                .Replace(Constant.TotalPremium, Constant.Pound + Math.Round(outPutDto.TotalPremium, 2).ToString())
+                .Replace(Constant.InitilaMonthPremium, Constant.Pound + outPutDto.InitialMonthlyPaymentAmount.ToString(CultureInfo.InvariantCulture))
+                .Replace(Constant.OtherMonthPremium, Constant.Pound + outPutDto.OtherMonthlyPaymentsAmount.ToString(CultureInfo.InvariantCulture));
                 myTempFilePath = Path.Combine(Path.GetTempPath(), outPutDto.CustomerId + "_" + outPutDto.FirstName + Constant.Extention);
                 using StreamWriter sw = new StreamWriter(myTempFilePath);
                 sw.WriteLine(sb);
